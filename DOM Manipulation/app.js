63b62ffed9413f802c8e847eb413420667d2116d
Souptik Koley <souptikmemari264@gmail.com>
2026-05-02 17:36:34 +0530

    dom manipulation

---
+++ DOM Manipulation/app.js
@@ -0,0 +1,15 @@
+// let smallimages=document.querySelectorAll("img");
+// for(let i=0;i<smallimages.length;i++){
+//     smallimages[i].src="spiderman_img.png";
+// }
+
+// let para=document.querySelector("p");
+// console.dir(para);
+// console.dir(para.innerHTML);
+// console.dir(para.innerText);
+// console.dir(para.textContent);
+// para.innerText="Hi I am Souptik Koley";
+// para.innerHTML="Hi i am <b>Peter parkar</b>";
+// let heading=document.querySelector("h1");
+// heading.innerHTML=`<u>${heading.innerText}</u>`;
+// heading.style.color="red";
