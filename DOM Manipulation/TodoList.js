f6bdcb660cf76921b8b7352c59280177e9458788
Souptik Koley <souptikmemari264@gmail.com>
2026-05-08 20:57:05 +0530

    changes

---
+++ DOM Manipulation/TodoList.js
@@ -0,0 +1,23 @@
+let button=document.querySelector("button");
+let input=document.querySelector("input");
+let ul=document.querySelector("ul");
+button.addEventListener("click",function(){
+    let items=document.createElement("li");
+    items.innerText=input.value;
+    let deletebutton=document.createElement("button");
+    deletebutton.innerText="Delete";
+    deletebutton.classList.add("delete");
+    ul.appendChild(items);
+    items.appendChild(deletebutton);
+    console.log(input.value);
+    input.value="";
+})
+let buttons=document.querySelectorAll(".delete");
+for(deletebutton of buttons){
+    deletebutton.addEventListener("click",function(){
+        console.log("delete");
+        let par=this.parentElement;
+        console.log(par);
+        par.remove();
+    })
+}
