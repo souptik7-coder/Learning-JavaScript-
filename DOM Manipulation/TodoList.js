5f7658ab4984189ed6c005f5d16279f652a707ec
Souptik Koley <souptikmemari264@gmail.com>
2026-05-08 21:10:55 +0530

    added

--- DOM Manipulation/TodoList.js
+++ DOM Manipulation/TodoList.js
@@ -12,12 +12,21 @@ button.addEventListener("click",function(){
     console.log(input.value);
     input.value="";
 })
-let buttons=document.querySelectorAll(".delete");
-for(deletebutton of buttons){
-    deletebutton.addEventListener("click",function(){
-        console.log("delete");
-        let par=this.parentElement;
-        console.log(par);
-        par.remove();
-    })
-}
+// let buttons=document.querySelectorAll(".delete");
+// for(deletebutton of buttons){
+//     deletebutton.addEventListener("click",function(){
+//         console.log("delete");
+//         let par=this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     })
+// }
+ul.addEventListener("click",function(){
+    console.log(event.target.nodeName);
+    if(event.target.nodeName=="BUTTON"){
+        console.log("deleted");
+        let listitems=event.target.parentElement;
+        console.log(listitems);
+        listitems.remove();
+    }
+})
