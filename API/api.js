cb5979c93b0f4efa538cabddc9e2fca8f47f5346
Souptik Koley <souptikmemari264@gmail.com>
2026-05-13 01:24:27 +0530

    api

--- API/api.js
+++ API/api.js
@@ -1,17 +1,37 @@
-let jsondata='{"fact":"In just 7 years, one un-spayed female cat and one un-neutered male cat and their offspring can result in 420,000 kittens.","length":121}';
+// let jsondata='{"fact":"In just 7 years, one un-spayed female cat and one un-neutered male cat and their offspring can result in 420,000 kittens.","length":121}';
 
-// JSON data to JS object
+// // JSON data to JS object
 
-let valid=JSON.parse(jsondata);
-console.log(valid);
-console.log(valid.fact);
+// let valid=JSON.parse(jsondata);
+// console.log(valid);
+// console.log(valid.fact);
 
 
-// JS object to JSON data
+// // JS object to JSON data
 
-let student={
-    name:"Souptik",
-    roll:44,
-    place:"Kolkata"
-}
-console.log(JSON.stringify(student));
+// let student={
+//     name:"Souptik",
+//     roll:44,
+//     place:"Kolkata"
+// }
+// console.log(JSON.stringify(student));
+
+let url="https://catfact.ninja/fact";
+fetch(url).then((res)=>{
+    console.log(res);
+   return res.json();
+})
+.then((data)=>{
+    console.log(data);
+    return fetch(url);
+})
+.then((res)=>{
+    console.log(res);
+    return res.json();
+})
+.then((data2)=>{
+    console.log(data2);
+})
+.catch((err)=>{
+    console.log(err);
+})
