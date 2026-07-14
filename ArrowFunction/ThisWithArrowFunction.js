56fc067abc276e7e647e4f02e24699c435c7cb0c
Souptik Koley <souptikmemari264@gmail.com>
2026-07-14 07:44:49 +0530

    this

--- ArrowFunction/ThisWithArrowFunction.js
+++ ArrowFunction/ThisWithArrowFunction.js
@@ -1,23 +1,23 @@
 // This with Arrow Function
 
 const student={
-    name:"Souptik",
-    marks:87,
-    prop:this, // global scope it's means window
+    name: "Souptik",
+    marks: 87,
+    prop: this, // global scope it's means window
     getname:function(){
         console.log(this);  // print student object
         return this.name;
     },
-    getmarks:()=>{    // arrow function 
+    getmarks: ()=>{    // arrow function 
         console.log(this);    // parent scope      // print windows
         return this.marks;    
     },
-    getinfo1:function(){
+    getinfo1: function(){
         setTimeout(()=>{
             console.log(this)    // student     because arrow function has no this   
         },2000);                               // arrow function always see parent objects          
     },
-    getinfo2:function(){
+    getinfo2: function(){
         setTimeout(function(){
             console.log(this)                  // window
         },2000);  
